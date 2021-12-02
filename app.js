c519637bb438611b1f42efa7f7a96a428e8e39ed
MaximShigapov <78402884+MaximShigapov@users.noreply.github.com>
2021-12-01 22:10:34 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let tl =gsap.timeline({
+    scrollTrigger:{
+        trigger: ".front-page",
+        start: "0%",
+        end: "100%",
+        markers: true,
+        scrub: true,
+        pin: true,
+    },
+});
+
+tl.fromTo(".front-page", {clipPath: "circle(10%)"},
+{clipPath: "circle(75%)"}
+);
+tl.fromTo(".logo",{scale:3}, {scale:0, opacity: 0});
+
+tl.fromTo(".title",{opacity:0}, {opacity:1});
+tl.fromTo(".sub-title",{opacity:0}, {opacity:1});
